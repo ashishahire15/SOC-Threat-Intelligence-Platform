9d2935cf90ae32ddb90fd47a6a6f58691b4d67c7
Ashish <92576884+ashishahire15@users.noreply.github.com>
2026-03-06 18:18:13 -0500

    Update realtime_monitor.py

--- realtime_monitor.py
+++ realtime_monitor.py
@@ -5,23 +5,16 @@ import time
 LOG_FILE = "/var/ossec/logs/alerts/alerts.json"
 
 def monitor_alerts():
-
     with open(LOG_FILE, "r") as file:
-
-        # move to end of file
         file.seek(0,2)
-
+        
         while True:
-
             line = file.readline()
-
             if not line:
                 time.sleep(1)
                 continue
-
             try:
                 alert = json.loads(line)
                 return alert
-
             except json.JSONDecodeError:
                 continue
